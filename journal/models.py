aff0ac6570769ef0dc77c3827b7c050d7cdbb553
mkkotlin <123.ultron.1234@gmail.com>
2025-05-16 13:06:19 +0530

    drf and jwt added

--- journal/models.py
+++ journal/models.py
@@ -1,3 +1,14 @@
 from django.db import models
-from  os import path
+from  django.contrib.auth.models import User
 # Create your models here.
+
+class Entry(models.Model):
+    user = models.ForeignKey(User, on_delete=models.CASCADE)
+    date = models.DateField(auto_now_add = True)
+    title = models.CharField(max_length=200)
+    mood = models.CharField(max_length = 20)
+    is_private = models.BooleanField(default = False)
+
+    def __str__(self):
+        return f"{self.title}({self.user.username})"
+    
