9549ff46ebcae4aae270ebdf5e35bea073defbea
mayank <123.ultron.1234@gmail.com>
2025-05-18 16:32:39 +0530

    cors updared

--- journal_backend/settings.py
+++ journal_backend/settings.py
@@ -54,7 +54,10 @@ MIDDLEWARE = [
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
     'corsheaders.middleware.CorsMiddleware',
 ]
-CORS_ALLOW_ALL_ORIGINS = True
+# CORS_ALLOW_ALL_ORIGINS = True
+CORS_ALLOWED_ORIGINS = ["http://localhost:4200"]
+CORS_ALLOWED_CREDENTIALS = True
+# Access-Control-Allow-Origin: 
 
 REST_FRAMEWORK = {
     'DEFAULT_AUTHENTICATION_CLASSES':(
